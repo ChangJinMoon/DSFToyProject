90987e38f34a89f54cb2c8c223730cbb8c7e49db
Jin <jin1004boy@naver.com>
2022-12-08 12:11:12 +0900

    2022/12/08 hot fix

--- src/main/java/com/toyproject/demo/filter/CorsFilter.java
+++ src/main/java/com/toyproject/demo/filter/CorsFilter.java
@@ -30,7 +30,7 @@ public class CorsFilter implements Filter {
         // response cors
         res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Allow-Methods","/**");
+        res.setHeader("Access-Control-Allow-Methods","**");
         res.setHeader("Access-Control-Max-Age", "3600");
         res.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
